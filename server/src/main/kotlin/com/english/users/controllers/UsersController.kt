367fff84b4a78db4883b70972f3657cc1af3d9f3
haxul <s.s.starodubov@yandex.ru>
2020-07-19 17:22:17 +0400

    add controllers

---
+++ server/src/main/kotlin/com/english/users/controllers/UsersController.kt
@@ -0,0 +1,25 @@
+package com.english.users.controllers
+
+import com.english.users.request.CreateUserRequest
+import com.english.users.response.CreateUserResponse
+import com.english.users.services.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/users")
+class UsersController {
+
+    @Autowired
+    lateinit var userService: UserService
+
+    @PostMapping
+    fun getUser(@Valid @RequestBody request: CreateUserRequest): CreateUserResponse {
+        val userId = userService.createUser(request.account, request.password)
+        return CreateUserResponse(request.account, userId)
+    }
+}
