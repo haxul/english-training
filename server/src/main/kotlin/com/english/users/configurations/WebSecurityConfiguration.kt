17193f3eb6e99ea4147e1e186307c6e287563b92
haxul <s.s.starodubov@yandex.ru>
2020-07-20 14:43:28 +0400

    add web security config

--- server/src/main/kotlin/com/english/users/configurations/WebSecurityConfiguration.kt
+++ server/src/main/kotlin/com/english/users/configurations/WebSecurityConfiguration.kt
@@ -1,5 +1,6 @@
 package com.english.users.configurations
 
+import com.english.users.security.AuthorizationFilter
 import com.english.users.services.UserService
 import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,13 +9,11 @@ import org.springframework.core.env.Environment
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.authentication.AuthenticationFilter
-
+import com.english.users.security.AuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
@@ -28,15 +27,17 @@ class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
     @Autowired
     private lateinit var environment: Environment
 
-    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users/register").permitAll()
-                .antMatchers(HttpMethod.POST, "/users/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-//                .addFilter(authenticationFilter)
-//                .addFilter(AuthorizationFilter(authenticationManager(), environment))
+                .addFilter(AuthenticationFilter(userService, authenticationManager(), environment))
+                .addFilter(AuthorizationFilter(authenticationManager(), environment))
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder)
     }
 }
