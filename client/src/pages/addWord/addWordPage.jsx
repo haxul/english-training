1546378971851c672f195fba81951c406351ec34
haxul <s.s.starodubov@yandex.ru>
2020-07-21 23:30:47 +0400

    fix: add google api

--- client/src/pages/addWord/addWordPage.jsx
+++ client/src/pages/addWord/addWordPage.jsx
@@ -23,6 +23,52 @@ async function saveWord(word, translation) {
     return null
 }
 
+async function translateByGoogle(russianWord) {
+    const token = localStorage.getItem("token")
+    if (!token) document.location.reload()
+
+    const body = {
+        "q": russianWord,
+        "target": "en",
+        "source": "ru"
+    }
+
+    const response = await fetch("https://google-translate1.p.rapidapi.com/language/translate/v2", {
+        "method": "POST",
+        "headers": {
+            "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+            "X-RapidAPI-Key": "868d1b1cbcmsh2b3edd9e06031f7p10b86bjsn649ef2f9010e",
+            "accept-encoding": "application/gzip",
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "body": JSON.stringify(body)
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+    fetch("https://google-translate1.p.rapidapi.com/language/translate/v2", {
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+            "x-rapidapi-key": "868d1b1cbcmsh2b3edd9e06031f7p10b86bjsn649ef2f9010e",
+            "accept-encoding": "application/gzip",
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "body": {
+            "source": "en",
+            "q": "Hello, world!",
+            "target": "es"
+        }
+    })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
 export function AddWordPage() {
     const [windowText, setWindowText] = useState("")
     const [translationText, setTranslationText] = useState("")
@@ -60,8 +106,8 @@ export function AddWordPage() {
         timer = setTimeout(() => getTranslation(text), 1000)
     }
 
-    const getTranslation = (phrase) => {
-        //TODO запрос на google translate
+    const getTranslation = async (phrase) => {
+        await translateByGoogle(phrase)
         setWindowText(phrase)
         setTimeout(() => setTranslationText(Math.random()), 1000)
     }
