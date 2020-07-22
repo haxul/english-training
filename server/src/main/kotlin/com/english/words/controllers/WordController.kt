38524641e04722328f4be7fa2cac16f1d92c4827
haxul <s.s.starodubov@yandex.ru>
2020-07-22 22:33:42 +0400

    amazon controller

--- server/src/main/kotlin/com/english/words/controllers/WordController.kt
+++ server/src/main/kotlin/com/english/words/controllers/WordController.kt
@@ -4,10 +4,18 @@ import com.english.users.configurations.SecurityContextHolder
 import com.english.users.exceptions.BadRequestException
 import com.english.words.CreateWordResponse
 import com.english.words.requests.CreateWordRequest
+import com.english.words.requests.TranslateAmazonRequest
 import com.english.words.requests.UpdateWordRequest
+import com.english.words.response.AmazonTranslationResponse
 import com.english.words.services.WordsService
+import com.google.gson.Gson
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import java.net.URI
+import java.net.URLEncoder
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import javax.validation.Valid
 
 @RestController
@@ -29,5 +37,16 @@ class WordController {
     fun updateWord(@Valid @RequestBody request: UpdateWordRequest):String {
         wordsService.updateWordValueById(request.value, request.translation, request.wordId)
         return "DONE"
+    }
+
+    @PostMapping("/amazon/translation")
+    fun translateWordInAmazon(@RequestBody request: TranslateAmazonRequest) : AmazonTranslationResponse {
+        val text = URLEncoder.encode(request.text, "utf-8")
+        val client = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+                .uri(URI.create("https://71ceo6bjqk.execute-api.us-east-2.amazonaws.com/default/translateRussian/?text=$text"))
+                .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return Gson().fromJson(response.body(), AmazonTranslationResponse::class.java)
     }
 }
